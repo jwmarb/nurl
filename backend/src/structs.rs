cc46c485febeaf8ac7767cca6e65c86bb8f24fe2
CoolPenguin42 <nemesist06@gmail.com>
2025-05-01 15:02:26 -0700

    added util structs and enum

---
+++ backend/src/structs.rs
@@ -0,0 +1,35 @@
+use chrono::{DateTime, Utc};
+
+// User model, holds their id + username + password
+#[derive(Debug, Clone)]
+pub struct User {
+    pub id: String,
+    pub username: String,
+
+    // bcrypt hash password
+    pub password: String,
+}
+
+// The URL shortener model itself
+#[derive(Debug, Clone)]
+pub struct ShortenedUrl {
+    pub id: String, // unique id
+    pub original_url: String, // orig long url
+    pub short_url: String, // new short url
+
+    pub expiry_date: Option<DateTime<Utc>>, // optional expiration date
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+
+    pub owner: String, // the id of the person that owns this
+    pub redirects: u64, // use count
+}
+
+/// Errors
+#[derive(Debug)]
+pub enum ShortenError {
+    NotFound,
+    Unauthorized,
+    InvalidInput(String),
+    DatabaseError(String),
+}
