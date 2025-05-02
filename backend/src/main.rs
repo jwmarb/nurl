6f7866a77de2590c5cbbd9f85bbd318a2f4dcb8a
Joseph Marbella <josephmarbella@arizona.edu>
2025-05-01 20:28:53 -0700

    refactor: move db init to `utils.rs`

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,17 +1,15 @@
 mod constants;
 mod routes;
+mod service;
 mod structs;
 mod utils;
-mod service;
 use actix_files::{Files, NamedFile};
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
-use constants::POSTGRESQL_URL;
 use constants::{FRONTEND_DIST, HOST, PORT};
 use dotenv::dotenv;
 use routes::health::health;
 use routes::register::register;
-use sqlx::postgres::PgPoolOptions;
-use utils::is_production;
+use utils::{init_db, is_production};
 
 #[get("/")]
 async fn development() -> impl Responder {
@@ -26,46 +24,13 @@ async fn index() -> NamedFile {
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
 
-    // database stuff
-    // connecting to database
-    let pool = PgPoolOptions::new()
-        .max_connections(5)
-        .acquire_timeout(std::time::Duration::from_secs(5))
-        .connect(*POSTGRESQL_URL)
-        .await
-        .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e.to_string()))?;
+    let pool = init_db().await.map(|p| web::Data::new(p))?;
 
-    // queries to the database
-    sqlx::query(
-        r#"
-        CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-    "#,
-    )
-    .execute(&pool)
-    .await
-    .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e.to_string()))?;
-
-    sqlx::query(
-        r#"
-        CREATE TABLE IF NOT EXISTS users (
-            id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-            username TEXT NOT NULL,
-            password TEXT NOT NULL
-        );
-        "#,
-    )
-    .execute(&pool)
-    .await
-    .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e.to_string()))?;
-
-    let pool_data = web::Data::new(pool);
-
-    // server stuff
     let server = HttpServer::new(move || {
         let mut app = App::new()
             .service(health)
             .service(register)
-            .app_data(pool_data.clone());
+            .app_data(pool.clone());
 
         if is_production() {
             // Serve the static HTML files if we are in production
--- backend/src/utils.rs
+++ backend/src/utils.rs
@@ -1,7 +1,59 @@
 use crate::constants::{ENVIRONMENT, POSTGRESQL_URL, PRODUCTION_ENV};
-use sqlx::{postgres::PgPoolOptions, PgPool};
+use sqlx::{
+    postgres::{PgPoolOptions, PgQueryResult},
+    Pool, Postgres,
+};
 
 #[inline]
 pub fn is_production() -> bool {
     *ENVIRONMENT == PRODUCTION_ENV
+}
+
+pub async fn init_db() -> Result<Pool<Postgres>, std::io::Error> {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .acquire_timeout(std::time::Duration::from_secs(5))
+        .connect(*POSTGRESQL_URL)
+        .await
+        .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e.to_string()))?;
+
+    let pool_ref = &pool;
+
+    let query = async |q: &str| -> Result<PgQueryResult, std::io::Error> {
+        let result = sqlx::query(q)
+            .execute(pool_ref)
+            .await
+            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e.to_string()))?;
+        Ok(result)
+    };
+
+    query(r#"CREATE EXTENSION IF NOT EXISTS "pgcrypto";"#).await?;
+    query(
+        r#"
+        CREATE TABLE IF NOT EXISTS users (
+            id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+            username TEXT NOT NULL,
+            password TEXT NOT NULL
+        );
+        "#,
+    )
+    .await?;
+    query(
+        r#"
+    CREATE TABLE IF NOT EXISTS shortened_urls (
+        id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+        original_url TEXT NOT NULL,
+        short_url TEXT NOT NULL UNIQUE,
+
+        expiry_date TIMESTAMPTZ,
+        created_at TIMESTAMPTZ NOT NULL,
+        updated_at TIMESTAMPTZ NOT NULL,
+
+        owner UUID NOT NULL REFERENCES users(id),
+        redirects BIGINT NOT NULL DEFAULT 0
+    );
+    "#,
+    )
+    .await?;
+    Ok(pool)
 }
